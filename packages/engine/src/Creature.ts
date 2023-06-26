4680838f4a22e74a493d5be9b467bc2e9d90c194
Nikolay Dorofeev <dorich2000@gmail.com>
2023-06-26 14:15:44 +0400

    Implement entity

--- packages/engine/src/Creature.ts
+++ packages/engine/src/Creature.ts
@@ -1,20 +1,15 @@
-import { Vector2 } from './Vector2';
 import { Position } from './Position';
 import { Direction } from './Direction';
 import { Container, type AnimatedSprite } from 'pixi.js';
-import { Collider } from './Collider';
+import { Entity } from './Entity';
 
 export type CreatureAnimations = {
   idle: AnimatedSprite;
   walk: AnimatedSprite;
 };
 
-export abstract class Creature {
+export abstract class Creature extends Entity {
   state: keyof CreatureAnimations = 'idle';
-  velocity: Vector2 = new Vector2(0, 0);
-  direction: Direction = Direction.RIGHT;
-  readonly container: Container = new Container();
-  protected ownVelocity: Vector2 = new Vector2(0, 0);
   private animations: CreatureAnimations;
 
   private currentSprite: AnimatedSprite | null = null;
@@ -24,41 +19,16 @@ export abstract class Creature {
     stage?: Container;
     animations: CreatureAnimations;
   }) {
+    super(options);
     this.animations = options.animations;
     this.switchAnimation('idle', { forceUpdate: true });
     this.currentSprite = this.animations.idle;
     this.container.pivot.x = this.container.width / 2;
     this.container.pivot.y = this.container.height;
-    if (options?.stage) {
-      options.stage.addChild(this.container);
-    }
-    if (options?.position) {
-      this.setPosition(options.position);
-    } else {
-      this.setPosition(new Position(0, 0));
-    }
-  }
-
-  get position() {
-    return new Position(
-      this.container.x,
-      this.container.parent?.height - this.container.y
-    );
-  }
-
-  get collider() {
-    return new Collider(
-      this.position,
-      Math.abs(this.container.width),
-      Math.abs(this.container.height)
-    );
   }
 
   onUpdate(delta: number) {
-    const newPosition = this.position.add(
-      this.ownVelocity.add(this.velocity).multiplyByScalar(delta)
-    );
-    this.setPosition(newPosition);
+    super.onUpdate(delta);
     if (this.ownVelocity.x > 0) {
       this.direction = Direction.RIGHT;
     } else if (this.ownVelocity.x < 0) {
@@ -66,27 +36,6 @@ export abstract class Creature {
     }
     this.container.scale.x = this.direction === Direction.RIGHT ? 1 : -1;
     this.switchAnimation(this.ownVelocity.x === 0 ? 'idle' : 'walk');
-  }
-
-  setPosition(position: Position) {
-    this.container.x = position.x;
-    this.container.y = this.container.parent?.height - position.y;
-  }
-
-  setVelocity(velocity: Vector2) {
-    this.velocity = velocity;
-  }
-
-  setVelocityX(x: number) {
-    this.velocity = new Vector2(x, this.velocity.y);
-  }
-
-  setVelocityY(y: number) {
-    this.velocity = new Vector2(this.velocity.x, y);
-  }
-
-  resetVelocity() {
-    this.velocity = new Vector2(0, 0);
   }
 
   protected switchAnimation(
---
+++ packages/engine/src/Entity.ts
@@ -0,0 +1,66 @@
+import { Vector2 } from './Vector2';
+import { Position } from './Position';
+import { Direction } from './Direction';
+import { Container, type AnimatedSprite } from 'pixi.js';
+import { Collider } from './Collider';
+
+export abstract class Entity {
+  velocity: Vector2 = new Vector2(0, 0);
+  direction: Direction = Direction.RIGHT;
+  readonly container: Container = new Container();
+  protected ownVelocity: Vector2 = new Vector2(0, 0);
+
+  constructor(options: { position?: Position; stage?: Container }) {
+    if (options?.stage) {
+      options.stage.addChild(this.container);
+    }
+    if (options?.position) {
+      this.setPosition(options.position);
+    } else {
+      this.setPosition(new Position(0, 0));
+    }
+  }
+
+  get position() {
+    return new Position(
+      this.container.x,
+      this.container.parent?.height - this.container.y
+    );
+  }
+
+  get collider() {
+    return new Collider(
+      this.position,
+      Math.abs(this.container.width),
+      Math.abs(this.container.height)
+    );
+  }
+
+  onUpdate(delta: number) {
+    const newPosition = this.position.add(
+      this.ownVelocity.add(this.velocity).multiplyByScalar(delta)
+    );
+    this.setPosition(newPosition);
+  }
+
+  setPosition(position: Position) {
+    this.container.x = position.x;
+    this.container.y = this.container.parent?.height - position.y;
+  }
+
+  setVelocity(velocity: Vector2) {
+    this.velocity = velocity;
+  }
+
+  setVelocityX(x: number) {
+    this.velocity = new Vector2(x, this.velocity.y);
+  }
+
+  setVelocityY(y: number) {
+    this.velocity = new Vector2(this.velocity.x, y);
+  }
+
+  resetVelocity() {
+    this.velocity = new Vector2(0, 0);
+  }
+}

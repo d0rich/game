e27e1bf6abf88a71d7c90e443e2d2764161522f9
Nikolay Dorofeev <dorich2000@gmail.com>
2023-07-02 15:53:50 +0400

    Setup background

--- apps/client/src/environment/Arena.ts
+++ apps/client/src/environment/Arena.ts
@@ -1,18 +1,28 @@
-import { Container, Graphics } from 'pixi.js';
+import { BaseTexture, Container, Graphics, Texture } from 'pixi.js';
 import { Ground } from './Ground';
 import { Position } from 'engine/src/physics';
 import { Entity } from 'engine/src/entities';
+import { getTexture } from 'utils/src/getTexture';
+import bg1 from 'assets/environment/industrial-zone/background/1.png';
+import bg2 from 'assets/environment/industrial-zone/background/2.png';
+import bg3 from 'assets/environment/industrial-zone/background/3.png';
+import bg4 from 'assets/environment/industrial-zone/background/4.png';
+import bg5 from 'assets/environment/industrial-zone/background/5.png';
+
+const bgTextures = {
+  bg1: await getTexture(bg1),
+  bg2: await getTexture(bg2),
+  bg3: await getTexture(bg3),
+  bg4: await getTexture(bg4),
+  bg5: await getTexture(bg5),
+};
 
 export class Arena {
   container = new Container();
   staticEntities: Entity[] = [];
 
   constructor() {
-    const fullScreenContainer = new Graphics();
-    fullScreenContainer.beginFill(0x000000);
-    fullScreenContainer.drawRect(0, 0, 640, 360);
-    fullScreenContainer.zIndex = -1;
-    this.container.addChild(fullScreenContainer);
+    this.setupBackground();
     this.setupStaticEntities();
   }
 
@@ -31,5 +41,20 @@ export class Arena {
         height: 128,
       })
     );
+  }
+
+  setupBackground() {
+    const setTexture = (texture: BaseTexture) => {
+      const bg = new Graphics();
+      bg.beginTextureFill({ texture: Texture.from(texture) });
+      bg.drawRect(0, 0, 640, 360);
+      bg.zIndex = -1;
+      this.container.addChild(bg);
+    };
+    setTexture(bgTextures.bg1);
+    setTexture(bgTextures.bg2);
+    setTexture(bgTextures.bg3);
+    setTexture(bgTextures.bg4);
+    setTexture(bgTextures.bg5);
   }
 }
